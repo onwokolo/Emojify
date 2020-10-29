7744a423acffac5223bde3746efb4c89c99a142a
jtaylor <jtaylor@seven.com>
2020-10-28 20:44:12 -0500

    Add Some Initial Setup

--- app/src/main/java/com/example/emojify/UI/Home/FirstFragment.kt
+++ app/src/main/java/com/example/emojify/UI/Home/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.example.emojify
+package com.example.emojify.UI.Home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.emojify.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
--- app/src/main/java/com/example/emojify/UI/Home/MainActivity.kt
+++ app/src/main/java/com/example/emojify/UI/Home/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.emojify
+package com.example.emojify.UI.Home
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.emojify.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
