783d975c5d4af3a724dc92509f73872adbe0c2b2
jtaylor <jtaylor@seven.com>
2020-11-20 22:11:40 -0600

    Fix camera rotation Add compression to bitmap for faster storage and less memory usage

--- app/src/main/java/com/example/emojify/storage/Entry.kt
+++ app/src/main/java/com/example/emojify/storage/Entry.kt
@@ -9,7 +9,7 @@ import java.io.ByteArrayOutputStream
 
 //Grab instance of me using "StorageSystem.storage"
 @kotlinx.serialization.Serializable
-class Entry(private val thumbnail: ByteArray, private val date: String, private val emotion: String) {
+class Entry(val thumbnail: ByteArray, val date: String, val emotion: String) {
     override fun equals(other: Any?): Boolean {
         other?.let {
             if (it.javaClass == this.javaClass) {
@@ -21,12 +21,18 @@ class Entry(private val thumbnail: ByteArray, private val date: String, private 
     }
 
     companion object {
-        fun convertImageToByteArray(imageView: ImageView): ByteArray {
-            val bitmapDrawable = imageView.drawable as BitmapDrawable
-            val bitmap = bitmapDrawable.bitmap
-            val stream = ByteArrayOutputStream()
-            return stream.toByteArray()
-
+        //grabs the image from the image view and converts it to a bitmap for storage
+        //compression=0 means MAX compression, while compression=100 means no compression, preserve original image
+        //It is recommended for 30-40 compression for minimum size while maintaining quality
+        fun convertImageToByteArray(imageView: ImageView, compression: Int): ByteArray {
+            imageView.drawable?.let {
+                val bitmapDrawable = it as BitmapDrawable
+                val bitmap = bitmapDrawable.bitmap
+                val stream = ByteArrayOutputStream()
+                bitmap.compress(Bitmap.CompressFormat.JPEG, compression, stream);
+                return stream.toByteArray()
+            }
+        return ByteArray(0)
         }
         private fun convertByteArrayToBitmap(b: ByteArray): Bitmap {
             return BitmapFactory.decodeByteArray(b, 0, b.size)
